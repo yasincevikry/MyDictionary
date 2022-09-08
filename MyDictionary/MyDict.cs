6bde5205fdff139ffd45e4e7255cbdc81c14d20f
Yasin ÇEVİK <190706037@st.maltepe.edu.tr>
2022-09-08 19:38:05 +0300

    Add project files.

---
+++ MyDictionary/MyDict.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDict<SKey, SValue>
+    {
+        SKey[] key;
+        SKey[] tempKey;
+        SValue[] value;
+        SValue[] tempValue;
+        public MyDict()
+        {
+            key = new SKey[0];
+            value = new SValue[0];
+        }
+        public void Add(SKey sKey, SValue sValue)
+        {
+            tempKey = key;
+            key = new SKey[key.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+            }
+            key[key.Length - 1] = sKey;
+
+            tempValue = value;
+            value = new SValue[value.Length + 1];
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                value[i] = tempValue[i];
+            }
+            value[value.Length - 1] = sValue;
+
+        }
+        public SKey[] Key
+        {
+            get { return key; }
+        }
+        public SValue[] Value
+        {
+            get { return value; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<int, string> myDict = new MyDict<int, string>();
+
+            myDict.Add(11, "Yasin");
+            myDict.Add(10, "İcardi");
+            myDict.Add(10, "Mertens");
+            myDict.Add(5, "Toreira");
+
+            
+        }
+    }
+}
